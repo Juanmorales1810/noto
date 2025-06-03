cd844b6d6257bc8ed282b93c819ef665cbd30c27
Juanmorales1810 <juan1810.jm@gmail.com>
2025-06-03 12:17:49 -0300

    Refactor: Simplificar la importación de Button y actualizar las definiciones de tipos en los componentes de la barra de herramientas

--- src/components/toolbars/undo.tsx
+++ src/components/toolbars/undo.tsx
@@ -3,7 +3,7 @@
 import { CornerUpLeft } from "lucide-react";
 import React from "react";
 
-import { Button, type ButtonProps } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import {
     Tooltip,
     TooltipContent,
@@ -12,35 +12,36 @@ import {
 import { cn } from "@/lib/utils";
 import { useToolbar } from "@/components/toolbars/toolbar-provider";
 
-const UndoToolbar = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ className, onClick, children, ...props }, ref) => {
-        const { editor } = useToolbar();
+const UndoToolbar = React.forwardRef<
+    HTMLButtonElement,
+    React.ComponentProps<"button">
+>(({ className, onClick, children, ...props }, ref) => {
+    const { editor } = useToolbar();
 
-        return (
-            <Tooltip>
-                <TooltipTrigger asChild>
-                    <Button
-                        variant="ghost"
-                        size="icon"
-                        className={cn("h-8 w-8", className)}
-                        onClick={(e) => {
-                            editor?.chain().focus().undo().run();
-                            onClick?.(e);
-                        }}
-                        disabled={!editor?.can().chain().focus().undo().run()}
-                        ref={ref}
-                        {...props}
-                    >
-                        {children || <CornerUpLeft className="h-4 w-4" />}
-                    </Button>
-                </TooltipTrigger>
-                <TooltipContent>
-                    <span>Undo</span>
-                </TooltipContent>
-            </Tooltip>
-        );
-    }
-);
+    return (
+        <Tooltip>
+            <TooltipTrigger asChild>
+                <Button
+                    variant="ghost"
+                    size="icon"
+                    className={cn("h-8 w-8", className)}
+                    onClick={(e) => {
+                        editor?.chain().focus().undo().run();
+                        onClick?.(e);
+                    }}
+                    disabled={!editor?.can().chain().focus().undo().run()}
+                    ref={ref}
+                    {...props}
+                >
+                    {children || <CornerUpLeft className="h-4 w-4" />}
+                </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+                <span>Undo</span>
+            </TooltipContent>
+        </Tooltip>
+    );
+});
 
 UndoToolbar.displayName = "UndoToolbar";
 
